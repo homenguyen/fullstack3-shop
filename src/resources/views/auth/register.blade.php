cd9afc91feca029b1a586ee7e2ef7bb79de33a7e
dongnguyen248 <dacdongdhv@gmail.com>
2020-05-05 22:51:41 +0700

    fix register

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -5,119 +5,53 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-            <div class="card bg-light">
-                <article class="card-body mx-auto" style="max-width: 400px;">
-                    <h4 class="card-title mt-3 text-center">Create Account</h4>
-                    <p class="text-center">Get started with your free account</p>
-                    <p>
-                        <a href="{{ route('loginGithub') }}" class="btn btn-block btn-github"> <i class="fab fa-github"></i>   Login via Github </a>
-                        <a  href="{{ route('loginFacebook') }}" class="btn btn-block btn-facebook"> <i class="fab fa-facebook-f"></i>   Login via Facebook</a>
-                        <a href="{{ route('loginGoogle') }}" class="btn btn-block btn-google"> <i class="fab fa-google"></i>  Login via Google</a>
-                    </p>
-                    <p class="divider-text">
-                        <span class="bg-light">OR</span>
-                    </p>
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <div class="form-group input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"> <i class="fa fa-user"></i> </span>
-                            </div>
-                            <input name="" class="form-control" placeholder="Full name" type="text">
-                        </div> <!-- form-group// -->
-                        <div class="form-group input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
-                            </div>
-                            <input name="" class="form-control" placeholder="Email address" type="email">
-                        </div> <!-- form-group// -->
-                        <div class="form-group input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
-                            </div>
-                            <input class="form-control" placeholder="Create password" type="password">
-                        </div> <!-- form-group// -->
-                        <div class="form-group input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
-                            </div>
-                            <input class="form-control" placeholder="Repeat password" type="password">
-                        </div> <!-- form-group// -->                                      
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-block"> Create Account  </button>
-                        </div> <!-- form-group// -->      
-                        <p class="text-center">Have an account? <a href="/login">Log In</a> </p>                                                                 
-                </form>
-                </article>
+                <div class="card bg-light">
+                    <article class="card-body mx-auto" style="max-width: 400px;">
+                        <h4 class="card-title mt-3 text-center">Create Account</h4>
+                        <p class="text-center">Get started with your free account</p>
+                        <p>
+                            <a href="{{ route('loginGithub') }}" class="btn btn-block btn-github"> <i class="fab fa-github"></i>   Login via Github </a>
+                            <a  href="{{ route('loginFacebook') }}" class="btn btn-block btn-facebook"> <i class="fab fa-facebook-f"></i>   Login via Facebook</a>
+                            <a href="{{ route('loginGoogle') }}" class="btn btn-block btn-google"> <i class="fab fa-google"></i>  Login via Google</a>
+                        </p>
+                        <p class="divider-text">
+                            <span class="bg-light">OR</span>
+                        </p>
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                                </div>
+                                <input name="name" class="form-control" placeholder="Full name" type="text">
+                            </div> <!-- form-group// -->
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+                                </div>
+                                <input name="email" class="form-control" placeholder="Email address" type="email">
+                            </div> <!-- form-group// -->
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                                </div>
+                                <input name="password" class="form-control" placeholder="Create password" type="password">
+                            </div> <!-- form-group// -->
+                            <div class="form-group input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                                </div>
+                                <input name="password_confirmation" class="form-control" placeholder="Repeat password" type="password">
+                            </div> <!-- form-group// -->                                      
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-block"> Create Account  </button>
+                            </div> <!-- form-group// -->      
+                            <p class="text-center">Have an account? <a href="/login">Log In</a> </p>                                                                 
+                        </form>
+                    </article>
                 </div> <!-- card.// -->
             </div>
-            <!-- <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div> -->
+            
         </div>
     </div>
 </div>
